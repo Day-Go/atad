256b00eb83a6b56ac7f3755e596ab1dc0df13047
Day-Go <lucarusso.521@gmail.com>
2024-08-21 22:12:02 +1000

    minimum working example of bloom filter

--- 02_bloom_filter/src/main.zig
+++ 02_bloom_filter/src/main.zig
@@ -1,13 +1,41 @@
 const std = @import("std");
 
 pub fn main() !void {
+    var filter: u128 = 0;
+    const filter_size: u7 = 127;
+    const k: u2 = 3;
+
     const set = [_]u8{ 'a', 'b', 'c' };
-
     for (set) |element| {
-        var hasher = std.hash.Wyhash.init(0);
-        std.hash.autoHash(&hasher, element);
-        const hash = hasher.final();
+        for (0..k) |i| {
+            const hash: u64 = generate_hash(element, i);
+            std.debug.print("Hash of '{c}': {b}\n", .{ element, hash });
 
-        std.debug.print("Hash of '{c}': {b}\n", .{ element, hash });
+            const index = @as(u7, @intCast(hash % filter_size));
+            filter |= (@as(u128, 1) << index);
+            std.debug.print("State of filter: {b}\n", .{filter});
+        }
+        std.debug.print("\n", .{});
+    }
+
+    const items_to_test = [_]u8{ 'a', 'd', 'b', 'e' };
+    for (items_to_test) |item| {
+        var is_member = true;
+        for (0..k) |i| {
+            const hash: u64 = generate_hash(item, i);
+            std.debug.print("Hash of '{c}': {b}\n", .{ item, hash });
+            const index = @as(u7, @intCast(hash % filter_size));
+            is_member = is_member and ((filter & (@as(u128, 1) << index)) != 0);
+        }
+        std.debug.print("Is '{c}' a member? {}\n", .{ item, is_member });
     }
 }
+
+const generate_hash = struct {
+    fn generate(element: u8, seed: usize) u64 {
+        var hasher = std.hash.Wyhash.init(seed);
+        std.hash.autoHash(&hasher, element);
+        const hash = hasher.final();
+        return hash;
+    }
+}.generate;
