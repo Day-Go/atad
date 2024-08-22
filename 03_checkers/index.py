d20643d3a4a22e2886c8dc968df6e6cf05777efa
Day-Go <lucarusso.521@gmail.com>
2024-08-22 22:01:37 +1000

    create board

--- 03_checkers/index.py
+++ 03_checkers/index.py
@@ -1,7 +1,18 @@
-from fasthtml.common import FastHTML
+from fasthtml.common import *
 
-app = FastHTML()
+css = Style('.square-dark{background-color: gray} .square-light{background-color: antiquewhite} :root{--pico-grid-column-gap: 0px; --pico-grid-row-gap: 0px}')
+
+app = FastHTML(hdrs=(picolink, css))
+
+SQUARE_SIZE = "100px"
+
+def create_row(start_with_dark, row):
+    colours = ['square-dark', 'square-light'] if start_with_dark else ['square-light', 'square-dark']
+    return [Div(id=f'b{row}{col}', cls=colours[col % 2], style=f'height: {SQUARE_SIZE};') for col in range(8)]
+
+rows = [Grid(*create_row(row % 2 == 0, row)) for row in range(8)]
+board = Div(*rows, style='width: 800px') 
 
 @app.get("/")
 def home():
-    return "<h1>Hello, World</h1>"
+    return Div(H1('Hello, World'), board)
